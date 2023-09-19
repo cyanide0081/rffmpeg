62d2723513c2e043175e48d689601e7215014e86
cyanide0081 <cyanide0081@gmail.com>
2023-09-19 01:15:55 -0300

    reformat help page

--- lib/help/help.h
+++ lib/help/help.h
@@ -4,33 +4,34 @@
 #include "../libs.h"
 
 static void displayHelp(void) {
-    printf(" Use me to automate an ffmpeg conversion inside a directory and its subfolders\n\n");
+    printf(" Use me to automate an ffmpeg conversion in one or more directories\n");
+    printf(" and organize its output automatically\n\n");
 
-    printf(" Arguments:\n");
-    printf("\t%-25sthe directory containing your input files (defaults to current working path if\
- left empty)\n", "-path <path(s)>");
-    printf("\t%-25sthe file format(s) you want to find and convert, separated by commas\
- (e.g.: mp4,mov)\n", "-in <format(s)>");
-    printf("\t%-25sthe ffmpeg conversion-related options (e.g.: -c:v libx264 -c:a aac -f\
- matroska)\n", "-opts <options>");
-    printf("\t%-25sthe extension of your output files (e.g.: mkv)\n\n", "-out <extension>");
+    printf(" ARGUMENTS:\n\n");
 
-    printf(" Additional flags:\n");
-    printf("\t%-25sstore converted files in a new subfolder created inside their respective\
- directories - you can choose a folder name with '--newfolder=foldername'\n",
-     OPT_NEWFOLDER_STRING);
-    printf("\t%-25sstore converted files in a separate directory - new or existing\
- (the syntax is the same as the newfolder option's)\n", OPT_NEWPATH_STRING);
-    printf("\t%-25sauto-overwrite existing output files (default behaviour appends an index)\n",
-     OPT_OVERWRITE_STRING);
-    printf("\t%-25sdelete original files after conversion (PERMANENT) \n", OPT_CLEANUP_STRING);
-    printf("\t%-25sdisable recursive subfolder search\n\n", OPT_NORECURSION_STRING);
 
-    printf(" Example: rffmpeg -path C:\\Users\\Music -in wav,aif -opts \"-c:a libmp3lame\"\
- -out mp3 --newfolder\n\n");
+    printf("\t%-20sthe file format(s) you want to find and convert,\n", "-i [FORMATS]");
+    printf("\t%-20sseparated by commas (e.g.: mp4,mov)\n", "");
+    printf("\t%-20sthe ffmpeg conversion-related options\n", "-p [PARAMS]");
+    printf("\t%-20s(e.g.: \"-c:v libx264 -c:a aac -f matroska\")\n", "");
+    printf("\t%-20sthe extension of your output files (e.g.: mkv)\n\n", "-o [FORMAT]");
 
-    printf(" (p.s.: you can run this as a console application by simply running it with no\
- arguments)\n\n");
+    printf(" NOTE: if you don't provide at least one input directory, it'll default to your\n current working directory\n\n");
+
+    printf(" ADDITIONAL FLAGS:\n\n");
+
+    printf("\t%-20sauto-overwrite existing output files\n", "-ow");
+    printf("\t%-20s(default behaviour appends an index)\n", "");
+    printf("\t%-20sdelete original files after conversion (PERMANENT) \n", "-cl");
+    printf("\t%-20sdisable recursive subfolder scanning\n", "-rn");
+    printf("\t%-20sstore converted files in a new subfolder\n", "-subfolder");
+    printf("\t%-20screated inside their respective directories\n", "");
+    printf("\t%-20syou can choose a name with '-subfolder=[NAME]')\n", "");
+    printf("\t%-20sstore converted files in a separate directory\n\n", "-newpath=[PATH]");
+
+    printf(" Example:\n\trffmpeg C:\\Users\\Music -i wav,aif -p \"-c:a libmp3lame -b:a 320k\" -o mp3\n -subfolder\n\n");
+
+    printf(" (p.s.: you can run this as a console application with no arguments)\n\n");
 }
 
 #endif // H_HELP_PAGE
