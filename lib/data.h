c297fe30c8328aebb4a5d1e545634eab76efd8e0
cyanide0081 <cyanide0081@gmail.com>
2023-09-27 13:48:00 -0300

    change minor versionnum

--- lib/data.h
+++ lib/data.h
@@ -44,7 +44,7 @@ typedef struct arguments {
     uint8_t options; // Bit fields for the optional arguments
 } arguments;
 
-#define PROGRAM_VERSION      "v1.1.0"
+#define PROGRAM_VERSION      "v1.1.1"
 #define CONSOLE_WINDOW_TITLE ("RFFmpeg " PROGRAM_VERSION)
 
 /* ANSI escape chars for colored shell output */
