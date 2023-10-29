add6e400d15d7ef9a8d03b6a89cd334765432f8d
cyanide0081 <cyanide0081@gmail.com>
2023-10-29 00:57:16 -0300

    change build script to sh lang

--- build.sh
+++ build.sh
@@ -1,40 +1,37 @@
-# build script for POSIX (linux/mac/freebsd/...) (clang)
-#!/usr/bin/env bash
+#!/bin/sh
+# build script for POSIX systems (Linux/MacOS/FreeBSD)
 
-# recursive file searching and compilation routine
-compile() {
-    local DEFINES=" -D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE"
-    local   FLAGS="-I./lib -std=c99 -lpthread $DEFINES -Wall -Wextra -pedantic -Wno-unused-function"
-    local D_FLAGS="-g"
-    local P_FLAGS="-finstrument-functions -ldl -rdynamic -DINSTRUMENTATION"
-    local R_FLAGS="-DNDEBUG -O2 -s"
-    local   files=""
+DEFINES=" -D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE"
+  FLAGS="-I./lib -std=c99 -lpthread $DEFINES -Wall -Wextra -pedantic -Wno-unused-function"
+D_FLAGS="-g"
+P_FLAGS="-finstrument-functions -ldl -rdynamic -DINSTRUMENTATION"
+R_FLAGS="-DNDEBUG -O2 -s"
 
-    for f in $(find . -name "*.c"); do
-        files="$files $f";
-    done
+files=""
+   CC="clang"
 
-    mkdir -p "./bin"
 
-    if [[ "$1" == "rel" ]]; then
-        echo "Building rffmpeg in RELEASE mode..."
-        echo
-        set -x
-        clang -o "bin/rffmpeg" $files $FLAGS $R_FLAGS
-    elif [[ "$1" == "prof" ]]; then
-        echo "Building rffmpeg in PROFILING mode..."
-        echo
-        set -x
-        clang -o "bin/rffmpeg" $files $FLAGS $D_FLAGS $P_FLAGS
-    else
-        echo "Building rffmpeg in DEBUG mode..."
-        echo
-        set -x
-        clang -o "bin/rffmpeg" $files $FLAGS $D_FLAGS
-    fi
+for f in $(find . -name "*.c"); do
+    files="$files $f";
+done
 
-    set +x
-}
+mkdir -p "./bin"
 
-# entry point of the script
-compile $1
+if [ "$1" == "rel" ]; then
+    echo "Building rffmpeg in RELEASE mode..."
+    echo
+    set -x
+    $CC -o "bin/rffmpeg" $files $FLAGS $R_FLAGS
+elif [ "$1" == "prof" ]; then
+    echo "Building rffmpeg in PROFILING mode..."
+    echo
+    set -x
+    $CC -o "bin/rffmpeg" $files $FLAGS $D_FLAGS $P_FLAGS
+else
+    echo "Building rffmpeg in DEBUG mode..."
+    echo
+    set -x
+    $CC -o "bin/rffmpeg" $files $FLAGS $D_FLAGS
+fi
+
+exit 0
