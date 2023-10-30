44345f65694f395cf3caa6ad21fe0ffc97679f36
cyanide0081 <cyanide0081@gmail.com>
2023-10-30 15:29:16 -0300

    remove bash-only parts of the shell script

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # build script for POSIX systems (Linux/MacOS/FreeBSD)
 
+CC="clang"
 DEFINES=" -D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE"
-  FLAGS="-I./lib -std=c99 -lpthread $DEFINES -Wall -Wextra -pedantic -Wno-unused-function"
-D_FLAGS="-g"
+FLAGS="-std=c99 -I./lib -pthread $DEFINES -Wall -Wextra -pedantic"
+D_FLAGS="-g -ggdb"
 P_FLAGS="-finstrument-functions -ldl -rdynamic -DINSTRUMENTATION"
 R_FLAGS="-DNDEBUG -O2 -s"
-
-files=""
-   CC="clang"
-
 
 for f in $(find . -name "*.c"); do
     files="$files $f";
@@ -17,21 +14,19 @@ done
 
 mkdir -p "./bin"
 
-if [ "$1" == "rel" ]; then
+if [ "$1" = "rel" ]; then
     echo "Building rffmpeg in RELEASE mode..."
     echo
     set -x
-    $CC -o "bin/rffmpeg" $files $FLAGS $R_FLAGS
-elif [ "$1" == "prof" ]; then
+    $CC -o "bin/rffmpeg" $files $FLAGS $R_FLAGS || exit 1
+elif [ "$1" = "prof" ]; then
     echo "Building rffmpeg in PROFILING mode..."
     echo
     set -x
-    $CC -o "bin/rffmpeg" $files $FLAGS $D_FLAGS $P_FLAGS
+    $CC -o "bin/rffmpeg" $files $FLAGS $D_FLAGS $P_FLAGS || exit 1
 else
     echo "Building rffmpeg in DEBUG mode..."
     echo
     set -x
-    $CC -o "bin/rffmpeg" $files $FLAGS $D_FLAGS
+    $CC -o "bin/rffmpeg" $files $FLAGS $D_FLAGS || exit 1
 fi
-
-exit 0
