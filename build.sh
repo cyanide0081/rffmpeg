f9d5449ae19f5fd8d87f4d0ee3ff19cc5d1e77d1
cyanide0081 <cyanide0081@gmail.com>
2023-10-31 07:46:35 -0300

    fix typo in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # build script for POSIX systems (Linux/MacOS/FreeBSD)
 
 CC="clang"
-DEFINES=" -D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE"
+DEFINES="-D_POSIX_C_SOURCE=200809L -D_DEFAULT_SOURCE"
 FLAGS="-std=c99 -I./lib -pthread $DEFINES -Wall -Wextra -pedantic"
 D_FLAGS="-g -ggdb"
 P_FLAGS="-finstrument-functions -ldl -rdynamic -DINSTRUMENTATION"
