56386a5032f6ad2b30ddc171c01f80922b3997e7
cyanide0081 <cyanide0081@gmail.com>
2023-11-12 19:58:23 -0300

    extend cpu architecture detection

--- lib/data.h
+++ lib/data.h
@@ -76,7 +76,7 @@ typedef struct Thread {
 
 #if defined __x86_64__ || defined __amd64__
 #define CUR_ARCH "x86_64"
-#elif defines __i386__
+#elif defined __i386__ || defined __i486__ || defined __i586__ || defined __i686__
 #define CUR_ARCH "x86"
 #elif defined __aarch64__
 #define CUR_ARCH "arm64"
